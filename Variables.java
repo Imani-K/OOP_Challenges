1fc8ed8c360086dae904a3a0b7c2b3881254cf10
Imani-K <imani.okoth@strathmore.edu>
2025-04-29 09:39:57 +0300

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        double price = 19.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Alice";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: $" + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
