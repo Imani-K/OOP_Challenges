894709f85612b2b913ddec1f8abff063f5fa0fbc
Imani-K <imani.okoth@strathmore.edu>
2025-04-29 09:21:48 +0300

    Create choices.java

---
+++ choices.java
@@ -0,0 +1,11 @@
+public class Choices {
+    public static void main(String[] args) {
+        int price = 20; // You can change this value to test different outputs
+
+        if (price < 25) {
+            System.out.println("I'll buy!");
+        } else {
+            System.out.println("Too expensive");
+        }
+    }
+}
